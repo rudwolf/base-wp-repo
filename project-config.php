c29963cf06d7e64c7c42cba99c4ad589966a48d2
Rudwolf <rudwolf@gmail.com>
2019-07-19 01:14:08 -0300

    Changed composer and config to use pt_BR version

--- project-config.php
+++ project-config.php
@@ -10,7 +10,7 @@ $table_prefix = 'wp_';
 // Language
 // Leave blank for American English
 // ================================
-define( 'WPLANG', '' );
+define( 'WPLANG', 'pt_BR' );
 
 // ==============================================================
 // Salts, for security
